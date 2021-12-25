9cbb130128846981db2cc116636712982158ccca
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2021-12-25 21:38:09 +0800

    Document class for base64-encoded string attributes

--- src/Base64String.php
+++ src/Base64String.php
@@ -4,11 +4,33 @@ namespace KennethTrecy\Elomocato;
 
 use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
 
+/**
+ * Automatically decodes the attrribute from or encodes the attribute into the database using native
+ * `base64_*` functions.
+ */
 class Base64String implements CastsAttributes {
+	/**
+	 * Decodes the original value using `base64_decode`.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Model $model
+	 * @param string $key
+	 * @param string $value
+	 * @param array $attributes
+	 * @return string
+	 */
 	public function get($model, $key, $value, $attributes) {
 		return base64_decode($value);
 	}
 
+	/**
+	 * Encodes the value to set using `base64_encode`.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Model $model
+	 * @param string $key
+	 * @param string $value
+	 * @param array $attributes
+	 * @return string
+	 */
 	public function set($model, $key, $value, $attributes) {
 		return base64_encode($value);
 	}
