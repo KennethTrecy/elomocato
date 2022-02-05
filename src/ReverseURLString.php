876c73885538950340b0b7dc930dc61d000caacf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-05 17:14:30 +0800

    Add class to reverse encode the strings

---
+++ src/ReverseURLString.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace KennethTrecy\Elomocato;
+
+use Illuminate\Contracts\Database\Eloquent\CastsAttributes;
+
+/**
+ * Automatically encodes the attribute from or decodes the attribute into the database using native
+ * `url**` functions.
+ */
+class ReverseURLString implements CastsAttributes {
+	/**
+	 * Encodes the original value using `urlencode`.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Model $model
+	 * @param string $key
+	 * @param string $value
+	 * @param array $attributes
+	 * @return string
+	 */
+	public function get($model, $key, $value, $attributes) {
+		return urlencode($value);
+	}
+
+	/**
+	 * Decodes the value to set using `base64_encode`.
+	 *
+	 * @param \Illuminate\Database\Eloquent\Model $model
+	 * @param string $key
+	 * @param string $value
+	 * @param array $attributes
+	 * @return string
+	 */
+	public function set($model, $key, $value, $attributes) {
+		return urldecode($value);
+	}
+}
