41d1d247be9ee9b0d769f8b730b100e283e7a0ce
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-08 15:06:59 +0800

    Refactor some tests

--- t/Feature/FriendlyDateTimeStringTest.php
+++ t/Feature/FriendlyDateTimeStringTest.php
@@ -10,6 +10,13 @@ use KennethTrecy\Elomocato\CastConfiguration;
 class MockDateFile extends File implements CastConfiguration {
 	protected $table = "files";
 
+	public static function createDefault() {
+		return static::create([
+			"name" => "a.txt",
+			"content" => "abc"
+		]);
+	}
+
 	protected $casts = [
 		"created_at" => FriendlyDateTimeString::class,
 		"updated_at" => FriendlyDateTimeString::class
@@ -29,10 +36,7 @@ class MockDateFile extends File implements CastConfiguration {
 class FriendlyDateTimeStringTest extends TestCase {
 	public function test_get() {
 		$now = now();
-		$model = MockDateFile::create([
-			"name" => "a.txt",
-			"content" => "abc"
-		]);
+		$model = MockDateFile::createDefault();
 
 		$this->assertDatabaseHas("files", [
 			"updated_at" => $now
@@ -42,10 +46,7 @@ class FriendlyDateTimeStringTest extends TestCase {
 
 	public function test_set() {
 		$now = now();
-		$model = MockDateFile::create([
-			"name" => "a.txt",
-			"content" => "abc"
-		]);
+		$model = MockDateFile::createDefault();
 		$updated_time = $now->addMinutes(3);
 
 		$model->updated_at = $updated_time;
@@ -59,10 +60,7 @@ class FriendlyDateTimeStringTest extends TestCase {
 
 	public function test_get_with_prefix() {
 		$now = now();
-		$model = MockDateFile::create([
-			"name" => "a.txt",
-			"content" => "abc"
-		]);
+		$model = MockDateFile::createDefault();
 
 		$this->assertDatabaseHas("files", [
 			"created_at" => $now
