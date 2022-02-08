353c63f684ea4dd2bbc6c70156d5f13c9aa8187f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-08 16:20:29 +0800

    Document cast configuration interface

--- src/CastConfiguration.php
+++ src/CastConfiguration.php
@@ -2,6 +2,15 @@
 
 namespace KennethTrecy\Elomocato;
 
+/**
+ * Interface that allows the custom cast classes to be configured dynamically.
+ */
 interface CastConfiguration {
+	/**
+	 * Returns an associated array which contains the configuration details of custom classes to
+	 * convert their respective associated attribute.
+	 *
+	 * @return array<string, mixed>
+	 */
 	public function getCastConfiguration();
 }
