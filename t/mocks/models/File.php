802428b72ad4d9544fbae4bf064120d0b81678b0
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-09 15:53:39 +0800

    Remove unnecessary code

--- t/mocks/models/File.php
+++ t/mocks/models/File.php
@@ -6,18 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class File extends Model {
 	protected $table = "files";
-	public $factory;
 
 	protected $fillable = [
 		"name",
 		"content"
 	];
-
-	function __constructor($casts) {
-		$this->casts = $casts;
-	}
-
-	protected static function newFactory() {
-		return $factory;
-	}
 }
