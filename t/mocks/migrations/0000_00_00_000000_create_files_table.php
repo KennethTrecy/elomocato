8723da1dfd216e5e67db6597a6e6b9725fa659c8
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-09 13:28:13 +0800

    Make file content nullable

--- t/mocks/migrations/0000_00_00_000000_create_files_table.php
+++ t/mocks/migrations/0000_00_00_000000_create_files_table.php
@@ -13,7 +13,7 @@ class CreateFilesTable extends Migration {
 	public function up() {
 		Schema::create("files", function (Blueprint $table) {
 			$table->id();
-			$table->string("name");
+			$table->string("name")->nullable();
 			$table->string("content");
 			$table->timestamps();
 		});
