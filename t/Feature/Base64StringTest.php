3c6718a63ab05c9b262455fdcdf5f37f269cf37f
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-09 14:50:16 +0800

    Separate the factory maker

--- t/Feature/Base64StringTest.php
+++ t/Feature/Base64StringTest.php
@@ -4,25 +4,18 @@ namespace Tests\Feature;
 
 use Tests\TestCase;
 use Tests\Mocks\Models\File;
-use Tests\Mocks\Models\FileFactory;
+use Tests\Helpers\CanMakeFactory;
 use KennethTrecy\Elomocato\Base64String;
 
 class Base64StringTest extends TestCase {
-	public function makeFactory() {
-		$class = new class extends File {
+	use CanMakeFactory;
+
+	private function createTestClass() {
+		return new class extends File {
 			protected $casts = [
 				"name" => Base64String::class
 			];
 		};
-
-		$class_name = get_class($class);
-		$factory = new class($class_name) extends FileFactory {
-			public function __construct($class_name) {
-				parent::__construct($class_name);
-			}
-		};
-
-		return $factory;
 	}
 
 	public function testGet() {
