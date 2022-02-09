d1719c37a11a7a90db07f6503fbd8c0e46461fdf
Kenneth Trecy Tobias <web.kennethtobias@gmail.com>
2022-02-09 15:19:37 +0800

    Refactor test for reverse URL string

--- t/Feature/ReverseURLStringTest.php
+++ t/Feature/ReverseURLStringTest.php
@@ -3,55 +3,47 @@
 namespace Tests\Feature;
 
 use Tests\TestCase;
+use Tests\Helpers\CanMakeFactory;
 use Tests\Mocks\Models\File;
 use KennethTrecy\Elomocato\ReverseURLString;
 
-class MockReverseURLFile extends File {
-	protected $table = "files";
-
-	protected $casts = [
-		"name" => ReverseURLString::class
-	];
-}
-
 class ReverseURLStringTest extends TestCase {
-	public function test_get() {
-		$name = "a a.text";
-		$encoded_name = urlencode($name);
-		$model = MockReverseURLFile::create([
-			"name" => $encoded_name,
-			"content" => "abc"
-		]);
+	use CanMakeFactory;
 
-		$this->assertDatabaseHas("files", [
-			"name" => $name
-		]);
-		$this->assertEquals($encoded_name, $model->name);
+	private function createTestClass() {
+		return new class extends File {
+			protected $casts = [
+				"name" => ReverseURLString::class
+			];
+		};
 	}
 
-	public function test_set() {
-		$old_name = "a a.txt";
-		$new_name = "b b.txt";
-		$model = MockReverseURLFile::create([
-			"name" => urlencode($old_name),
-			"content" => "abc"
+	public function testGet() {
+		$model = $this->makeFactory()->create();
+
+		$name = $model->name;
+
+		$this->assertDatabaseHas("files", [
+			"name" => urldecode($name)
 		]);
+	}
+
+	public function testSet() {
+		$model = $this->makeFactory()->create();
+		$updated_model = $this->makeFactory()->make();
+		$new_name = $updated_model->name;
 
 		$model->name = $new_name;
 		$model->save();
 
 		$this->assertDatabaseHas("files", [
-			"name" => $new_name
+			"name" => urldecode($new_name)
 		]);
-		$this->assertEquals(urlencode($new_name), $model->name);
 	}
 
-	public function test_null_set() {
+	public function testNullSet() {
 		$name = "a a.txt";
-		$model = MockReverseURLFile::create([
-			"name" => urlencode($name),
-			"content" => "abc"
-		]);
+		$model = $this->makeFactory()->create();
 
 		$model->name = null;
 		$model->save();
